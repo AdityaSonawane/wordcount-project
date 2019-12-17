a32e17dd3a21d6deec33d5ec66366804ba7990f8
AdityaSonawane <adits8p@gmail.com>
2019-12-17 20:32:53 +0530

    My first commit

---
+++ wordcount/urls.py
@@ -0,0 +1,12 @@
+
+
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.homepage,name='home'),
+    path('count/', views.count, name='count'),
+    path('about/', views.about, name='about'),
+]
